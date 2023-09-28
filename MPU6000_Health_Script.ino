0f78c592c71dd1a55c0c43d81938e7056e7b001b
jack-mcd <jamc5396@colorado.edu>
2023-09-27 18:42:17 -0600

    not deprecated anymore

--- MPU6000_Health_Script.ino
+++ MPU6000_Health_Script.ino
@@ -1,12 +1,22 @@
 /*
-Deprecated
+  Description: Accelerometer sample rate test
+  Author: Jack McDonald
+  Date: 9/27/23
 */
 
 #include "MPU6000.h"
 
 uint8_t divisor = 255; //possible that this must start from 254
+int idx;
+
 MPU6000 accelerometer(1, false); // Sets sensor ID to 1 and debugging to false
 sensor_float_vec_t acc; // Saves acceleration readings in a vector structure
+
+unsigned long startSampleLoop;
+unsigned long endSampleLoop;
+unsigned long sampleLoopTotal[17];
+
+int numSamples;
 
 void setup() {
 
@@ -21,13 +31,27 @@ void setup() {
 }
 
 void loop() {
-  int numSamples = 0;
+  numSamples = 0;
+
   Serial.print("Sample rate divisor: "); Serial.print(divisor); Serial.println();
 
   accelerometer.setSampleRateDivisor(divisor);
 
-  //This code was already written so we don't need it anymore
-  while (numSamples )
+  startSampleLoop = micros();
+  while (numSamples < 100) {
+    acc = accelerometer.getSample();
+    numSamples++;
+  }
+  endSampleLoop = micros();
+  /*
+  Design decision to trade memory for a more accurate time stamp of time it takes to complete while loop.
+  Possible that it is better to not have endSampleLoop variable and compute sampleLoopTotal as
+  sampleLoopTotal = micros() - startSampleLoop, but it was hypothesized that this micros() reading would
+  be less accurate than the way we are doing it above.
+  */
+  idx = (divisor/15) % 17;
+  sampleLoopTotal[idx] = endSampleLoop - startSampleLoop;
+  Serial.print("Time in microseconds: "); Serial.print(sampleLoopTotal[idx]); Serial.println();
 
   if (divisor == 0) {
     Serial.println("Highest sample rate reached");
