9377b3f0e177753b35664570d8e6e0e90126e0bf
jack-mcd <jamc5396@colorado.edu>
2023-10-02 11:10:16 -0600

    more accurate time

--- MPU6000_Health_Script.ino
+++ MPU6000_Health_Script.ino
@@ -16,7 +16,10 @@ unsigned long startSampleLoop;
 unsigned long endSampleLoop;
 unsigned long sampleLoopTotal[17];
 
+unsigned long loopTime;
+
 int numSamples;
+
 
 void setup() {
 
@@ -26,7 +29,28 @@ void setup() {
   accelerometer.begin(); // Begins transmission to the I2C slave device
   accelerometer.initialize(); // Set-up for MPU 
   accelerometer.setAccelRange(MPU6000_RANGE_2_G); // Sets range of acccelrometer 
-                                                    // Range options: 2_G, 4_G, 8_G, 16_G                      
+                                                    // Range options: 2_G, 4_G, 8_G, 16_G   
+  
+  /*
+  Find out time loop takes and time it takes to iterate numSamples as this needs to be subtracted from the actual
+  sample rate time that we calculate in loop(). An example to portray the logic behind this:
+
+  while (numSamples < 100) { <--- 2-3 operations: it's a while loop, check condition, continue or break
+    acc = accelerometer.getSample(); <---- ? operations: this is what we want to measure the time of
+    numSamples++; <----- ~2 operations: read current value, iterate this value and save it... we don't want this to be measured
+  }
+
+  Therefore, measure the time of the loop without the accelerometer readings, so we can subtract the time that takes below
+  to just get the time it takes to measure the accelerometer 100 times
+  */
+  int numSamples = 0;
+  startSampleLoop = micros();
+  while (numSamples < 100) {
+    numSamples++;
+  }
+  endSampleLoop = micros();
+  loopTime = endSampleLoop - startSampleLoop;
+  Serial.print("Loop time: "); Serial.print(loopTime); Serial.println();
 
 }
 
@@ -54,7 +78,8 @@ void loop() {
   be less accurate than the way we are doing it above.
   */
   idx = (divisor/15) % 17;
-  sampleLoopTotal[idx] = endSampleLoop - startSampleLoop;
+  Serial.print("For index "); Serial.print(idx); Serial.println();
+  sampleLoopTotal[idx] = (endSampleLoop - startSampleLoop) - loopTime;
   Serial.print("Time in microseconds: "); Serial.print(sampleLoopTotal[idx]); Serial.println();
 
   if (divisor == 0) {
