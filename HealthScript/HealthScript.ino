aa90761307d248352cf39ce1aeadef90ac851748
Jack McDonald <54564147+jack-mcd@users.noreply.github.com>
2023-10-23 11:24:29 -0600

    check all sensors' connections

--- HealthScript/HealthScript.ino
+++ HealthScript/HealthScript.ino
@@ -1,14 +1,40 @@
 #include "HealthScript.h"
-#include "Adafruit_TMP117.h"
 
 Adafruit_TMP117 temp;
+MPU6000 acc(1, false);
+SFE_MMC5983MA myMag;
+TPIS1385 therm(1);
+LoRaFi lora;
 HealthScript hs;
 
 void setup() {
   Serial.begin(9600);
   temp.begin();
-  Serial.println(hs.isInitialized());
-  Serial.println(hs.isTMP117Connected());
+  acc.begin();
+  acc.initialize();
+  acc.setAccelRange(MPU6000_RANGE_2_G);
+  myMag.begin();
+  therm.begin();
+  lora.begin();
+  if (hs.isInitialized()) {
+    Serial.println("Health script initialized");
+  }
+  if (hs.isTMP117Connected(temp)) {
+    Serial.println("TMP117 found");
+  }
+  if (hs.isMPU6000Connected(acc)) {
+    Serial.println("MPU6000 found");
+  }
+  if (hs.isMMC5983MAConnected(myMag)) {
+    Serial.println("MMC5983MA found");
+  }
+  if (hs.isTPiS1S1385Connected(therm)) {
+    Serial.println("TPiS 1S 1385 found");
+  }
+  if (hs.isSX1272Connected(lora)) {
+    Serial.println("SX1272 found");
+  }
+
 }
 
 void loop() {}
