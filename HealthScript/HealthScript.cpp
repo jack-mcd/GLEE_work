e77feeb2453f23c1840651b9d64be14cfbf5c432
Jack McDonald <54564147+jack-mcd@users.noreply.github.com>
2023-10-23 11:25:33 -0600

    Update HealthScript.cpp

--- HealthScript/HealthScript.cpp
+++ HealthScript/HealthScript.cpp
@@ -22,7 +22,22 @@ bool HealthScript::isInitialized() {
 /*
 Checks if TMP117 sensor is connected
 */
-bool HealthScript::isTMP117Connected() {
-    Adafruit_TMP117 temp;
+bool HealthScript::isTMP117Connected(Adafruit_TMP117 temp) {
     return temp.afterInitCheck();
+}
+
+bool HealthScript::isMPU6000Connected(MPU6000 acc) {
+    return acc.isConnected();
+}
+
+bool HealthScript::isMMC5983MAConnected(SFE_MMC5983MA mag) {
+    return mag.isConnected();
+}
+
+bool HealthScript::isTPiS1S1385Connected(TPIS1385 therm) {
+    return therm.isConnected();
+}
+
+bool HealthScript::isSX1272Connected(LoRaFi lora) {
+    return lora.isOn();
 }
