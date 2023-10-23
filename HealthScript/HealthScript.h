45b20c1873e5ced3d1b6771e613d7e6227dce09e
Jack McDonald <54564147+jack-mcd@users.noreply.github.com>
2023-10-23 11:46:07 -0600

    Update HealthScript.h

--- HealthScript/HealthScript.h
+++ HealthScript/HealthScript.h
@@ -12,15 +12,25 @@
 class HealthScript {
     public:
         HealthScript(); //default constructor sets all sensors to "off" (logically)
+
+        // void readBitString() TO-DO
+
         bool isInitialized(); //verifies our constructor has initialized private vars
         bool isTMP117Connected(Adafruit_TMP117 tmp); //checks if TMP117 sensor is connected
-        bool isMPU6000Connected(MPU6000 acc);
-        bool isMMC5983MAConnected(SFE_MMC5983MA mag);
-        bool isTPiS1S1385Connected(TPIS1385 therm);
-        bool isSX1272Connected(LoRaFi lora);
+        bool isMPU6000Connected(MPU6000 acc); //checks if MPU6000 sensor is connected
+        bool isMMC5983MAConnected(SFE_MMC5983MA mag); //checks if MMC5983MA sensor is connected
+        bool isTPiS1S1385Connected(TPIS1385 therm); //checks if TPIS1S1385 sensor is connected
+        bool isSX1272Connected(LoRaFi lora); //checks if SX1272 LoRa module is on
 
     private:
-        bool tmp; //false = off, true = on
+        /*
+        sensors[0] = TMP117 status
+        sensors[1] = MPU6000 status
+        sensors[2] = MMC5983MA status
+        sensors[3] = TPiS1S1385 status
+        sensors[4] = SX1272 status
+        */
+        bool sensors[5];
 };
 
 #endif
